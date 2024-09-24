a22e7a7f2b9115a04e0a7bb9d0b80a56f916e207
Paulo guilherme <pauloguilhermeferreiraoliv@gmail.com>
2024-09-24 16:41:12 -0300

    Opitional

---
+++ src/JavaCore/ZZJoptional/Classe/Seguradora.java
@@ -0,0 +1,29 @@
+package JavaCore.ZZJoptional.Classe;
+
+import java.util.Optional;
+
+public class Seguradora {
+    private String nome;
+
+    public Seguradora(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Seguradora{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
+    public Seguradora() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
