4fe43f2378b6a10fc42e246c519c875dd109ce90
PauloGuilhermeF <pauloguilhermeferreiraoliv@gmail.com>
2024-10-31 17:19:06 -0300

    Arrays

---
+++ src/Introducao/Arrays/Arrays4.java
@@ -0,0 +1,10 @@
+package Introducao.Arrays;
+
+public class Arrays4 {
+    public static void main(String[] args) {
+        String[] nomes = {"Juju", "Jao Jao", "Xu xu"};
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+    }
+}
