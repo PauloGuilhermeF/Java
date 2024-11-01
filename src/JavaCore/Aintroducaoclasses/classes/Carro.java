02ed03a5380a2b05a235032758aad22e354e0706
PauloGuilhermeF <pauloguilhermeferreiraoliv@gmail.com>
2024-11-01 16:34:18 -0300

    Introducao Classes

---
+++ src/JavaCore/Aintroducaoclasses/classes/Carro.java
@@ -0,0 +1,9 @@
+package JavaCore.Aintroducaoclasses.classes;
+
+
+public class Carro {
+    public String placa;
+    public String modelo;
+    public float velocidadeMaxima;
+
+}
---
+++ src/JavaCore/Aintroducaoclasses/test/CarroTest.java
@@ -0,0 +1,17 @@
+package JavaCore.Aintroducaoclasses.test;
+
+
+import JavaCore.Aintroducaoclasses.classes.Carro;
+
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.modelo = "Fusca";
+        carro.placa = "QWJ1029";
+        carro.velocidadeMaxima = 120f;
+        System.out.println(carro.placa);
+        System.out.println(carro.modelo);
+        System.out.println(carro.velocidadeMaxima);
+    }
+}
